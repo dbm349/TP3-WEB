78d491f28af6373af66141f952b126a031566d94
dbm349 <delmore97@hotmail.com>
2020-05-06 15:30:00 -0300

    readme

--- Ejercicio 1/app/controllers/NuevoTurnoController.php
+++ Ejercicio 1/app/controllers/NuevoTurnoController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 use App\Core\Controller;
 use App\Models\Turnos;
 
-$extensiones = array(0=>'image/jpeg',1=>'image/png');
+
 
 class NuevoTurnoController extends Controller
 {
@@ -19,148 +19,115 @@ class NuevoTurnoController extends Controller
      */
     public function index()
     {
-        $tasks = $this->model->get();
+        $turnos = $this->model->get();
         return view('turnos', compact('turnos'));
     }
 
     
 
-    public function create()
-    {
-       
-        $dato= array(
-            'numeroTurno'=> $numeroTurno,
-            'nombre'=> $_POST['nombre'],
-            'email'=> $_POST['email'],
-            'tel'=> $_POST['tel'],
-            'edad'=> $_POST['edad'],
-            'talla'=> $_POST['talla'],
-            'altura'=> $_POST['altura'],
-            'nacimiento'=> $_POST['nacimiento'],
-            'cpelo'=> $_POST['cpelo'],
-            'fechaturno'=> $_POST['fechaturno'],
-            'horaturno'=> $_POST['horaturno'],
-            'diagnostico' => $Nom_imagen,
-        );
+    public function create() {       
 
+        return views('turnos.create');
+            $dato= array(
+                'numeroTurno'=> $numeroTurno,
+                'nombre'=> $_POST['nombre'],
+                'email'=> $_POST['email'],
+                'tel'=> $_POST['tel'],
+                'edad'=> $_POST['edad'],
+                'talla'=> $_POST['talla'],
+                'altura'=> $_POST['altura'],
+                'nacimiento'=> $_POST['nacimiento'],
+                'cpelo'=> $_POST['cpelo'],
+                'fechaturno'=> $_POST['fechaturno'],
+                'horaturno'=> $_POST['horaturno'],
+            );
 
+            $array_data[]=$dato;
+            $datos_finales= json_encode($array_data);
+                       
+            $extensiones = array(0=>'image/jpeg',1=>'image/png');
+     
+            $chequeo = true;        
+            if($chequeo == true) {
+                if(empty($dato['nombre'])){
+                    $chequeo = false; 
+                } elseif (!preg_match('/^[a-zA-ZÀ-ž\s]+$/', $dato['nombre'])) {
+                    $chequeo = false;
+                }
+                
+                if(empty($dato['email'])){
+                    $chequeo = false; 
+                } elseif(!filter_var($dato['email'], FILTER_VALIDATE_EMAIL)) {
+                    $chequeo = false; 
+                }
+                
+                if(empty($dato['tel'])){
+                    $chequeo = false;
+                }elseif(!is_numeric($extra['tel'])){
+                    $chequeo = false;
+                }    
 
-                    if(empty($nombre)){
-                        echo "<p class='error'> * Campo nombre incompleto </p>";
-                    }else{
-                        if(!preg_match("/^[A-Za-zÀ-ž\s]*$/",$nombre)){
-                            echo "<p class='error'> * El nombre solo debe contener letras</p>";
-                        }
-                    }
+                if(empty($dato['nacimiento'])){
+                    $chequeo = false;
+                }
                 
-                    if(empty($email)){
-                        echo "<p class='error'> * Campo email incompleto </p>";
-                    }else{
-                        if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-                        echo "<p class='error'> * Email incorrecto </p>";
-                    }
-                
-        
-                    if(empty($tel)){
-                        echo "<p class='error'> * Campo teléfono incompleto </p>";
-                    }else{
-                        if(!is_numeric($tel)){
-                            echo "<p class='error'> * Solo se permiten números </p>";
-                        }
-                    }
-                
-                    if((!is_numeric($edad))){
-                        echo "<p  class='error'> *La edad debe ser numérica </p>";
-                    }else{
-                        if(($edad<1) || ($edad>105)){
-                            echo "<p class='error'> * La edad debe ser mayor a 1 y menor a 105 </p>";
-                        }
-                    }
-        
-        
-                    if((!preg_match("/^(0[8-9]|1[0-6]):(00|15|30|45)$/",$horaturno)) &&
-                             (!preg_match("/^(17):(00)$/",$horaturno))){
-                        echo "<p class='error'> * Turno no válido </p>";
-                    }
-                    
-                    
-                    if(empty($nacimiento)){
-                        echo "<p class='error'> * Campo fecha de nacimiento incompleto </p>";
-                    }elseif(!preg_match("/^([0-9][0-9][0-9][0-9])-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])$/",$nacimiento)){
-                            echo "<p class='error'> * Formato de fecha inválida </p>";
-                    
-                    }elseif($fnacimiento>$fechaactual){
-                            echo "<p class='error'> * ERROR! Fecha de nacimiento incorrecta</p>";
-                    }
-                    
-                
-        
-        
-        
-                    if(empty($fechaturno)){
-                        echo "<p class='error'> * Campo fecha de turno incompleto </p>";
-                    }elseif(!preg_match("/^([0-9][0-9][0-9][0-9])-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])$/",$nacimiento)){
-                            echo "<p class='error'> * Formato de fecha inválida </p>";
-                        
-                    }elseif($fturno<$fechaactual){
-                            echo "<p class='error'> * La fecha del turno debe ser mayor o igual a la actual</p>";
-                    }
-                    
-                
-        
-                    if((!is_numeric($altura))){
-                        echo "<p> *La altura debe ser numérica </p>";
-                    }elseif(($altura<0.5) || ($altura>2)){
-                            echo "<p class='error'> * La altura debe ser mayor a 0.5 y menor a 2 </p>";
-                        }
-                        
-                        
-                    
-                    if((!is_numeric($talla))){
-                        echo "<p> *La altura debe ser numérica </p>";
-                    }else{
-        
-                        if(($talla<20) || ($talla>45)){
-                            echo "<p class='error'> * La talla debe ser mayor a 20 y menor a 45 </p>";
-                        }
-                    } 
-        
-        
-                    if(($cpelo !="Castaño") &&
-                    ($cpelo !="Rubio") &&
-                    ($cpelo !="Pelirrojo") &&
-                    ($cpelo !="Negro")){
-        
-                        echo "<p class='error'> * Color de pelo incorrecto </p>";
-                    }
-                   
+                if(empty($dato['fechaturno'])){
+                    $chequeo = false;
+                }
 
-                    if(in_array($_FILES['diagnostico']['type'],$extensiones)){
-                        $indexphp = dirname(realpath(__FILE__), 2);
-                        $ruta_origen = $_FILES['diagnostico']['tmp_name'];
-                      
-                        $extensionArchivo = substr($_FILES['diagnostico']['name'],strlen($_FILES['diagnostico']['name'])-4,strlen($_FILES['diagnostico']['name']));
-                        $horaNueva = str_replace(':','-',$dato['horaturno']);
-                        
-                        
-                        //uso el nombre del paciente, la fecha del turno y la hora para no reemplazar una foto con el mismo nombre
-                        
-                        //Reemplazar los espacios en el nombre por guiones
-                        $nombreLimpio = preg_replace('/\s+/', '-', $dato['nombre']);
-                        
-                        $nombreArchivo = $nombreLimpio.'-'.$dato['fechaturno'].'-'.$horaNueva.$extensionArchivo;
-                        $ruta_nueva = $indexphp . '\\Diagnosticos\\' . $nombreArchivo; 
-                        if (move_uploaded_file($ruta_origen,$ruta_nueva)){ 
-                                $dirImagen = './Diagnosticos/'.$nombreArchivo;
-                        }
-                     }else{
-                        $nombreArchivo = "";
-                        $dirImagen = "";
-                     } 
+                if(empty($dato['talla'])) {
+                } elseif (!is_numeric($extra['talla'])){
+                    $chequeo = false;
+                } elseif (($dato['talla']<20) || ($dato['talla']>45)){
+                    $chequeo = false;
+                }
+                
+            }
+            
+            $ruta_nueva = "";
+            if ($chequeo == true) {
+                if(in_array($_FILES['diagnostico']['type'],$extensiones)){
+                   $indexphp = dirname(realpath(__FILE__), 2);
+                   $ruta_origen = $_FILES['diagnostico']['tmp_name'];
                  
-
-
-        $this->model->insert($dato);
-        return redirect('turnos');
+                   $extensionArchivo = substr($_FILES['diagnostico']['name'],strlen($_FILES['diagnostico']['name'])-4,strlen($_FILES['diagnostico']['name']));
+                   $horaNueva = str_replace(':','-',$dato['horaturno']);
+                   
+                   
+                   //uso el nombre del paciente, la fecha del turno y la hora para no reemplazar una foto con el mismo nombre
+                   
+                   //Reemplazar los espacios en el nombre por guiones
+                   $nombreLimpio = preg_replace('/\s+/', '-', $dato['nombre']);
+                   
+                   $nombreArchivo = $nombreLimpio.'-'.$extra['fechaturno'].'-'.$horaNueva.$extensionArchivo;
+                   $ruta_nueva = $indexphp . '\\Diagnosticos\\' . $nombreArchivo; 
+                   if (move_uploaded_file($ruta_origen,$ruta_nueva)){ 
+                           $dirImagen = './Diagnosticos/'.$nombreArchivo;
+                   }
+                }else{
+                   $nombreArchivo = "";
+                   $dirImagen = "";
+                } 
+            }
+             else {
+                echo 'Campos erroneos';
+        
+                $this->model->insert($dato);
+                return redirect('turnos');    
+            }           
+   
+        } 
+           
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
